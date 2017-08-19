d054c69414f459ec268d023d2387d55979c6c557
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2017-08-19 21:11:17 +0200

    Change default ports to avoid conflicts

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -34,6 +34,12 @@ from evennia.settings_default import *
 # This is the name of your game. Make it catchy!
 SERVERNAME = "Awaregame"
 
+# Change ports
+TELNET_PORTS = [22000]
+WEBSERVER_ENABLED = False
+WEBCLIENT_ENABLED = False
+WEBSOCKET_CLIENT_ENABLED = False
+AMP_PORT = 22001
 
 ######################################################################
 # Settings given in secret_settings.py override those in this file.
